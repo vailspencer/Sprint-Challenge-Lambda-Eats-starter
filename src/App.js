94689a28677cd3470f2eefd694f5fd834858a35b
Spencer_Vail <vail.spencer@gmail.com>
2020-04-24 09:04:55 -0700

    Routes added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import {Route, Link} from 'react-router-dom'
 import Form from './components/Form'
 import Home from './components/Home'
+import styled from 'styled-components'
 
 const App = () => {
+
+
+  const Navigation =styled.div `
+  display:flex;
+  `
   return (
     <>
+    <Navigation>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <Link to ='/'>
+          <button>Home</button>
+        </Link>
+        <Link to ='/pizza'>
+          <button id='pizzaNav'>Pizza</button>
+        </Link>
+      </nav>
+    </Navigation>
     </>
   );
 };
